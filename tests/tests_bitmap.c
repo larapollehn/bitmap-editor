094153659789131160d37a63fb0799c1f3f4e9c4
larapollehn <lara.pollehn@googlemail.com>
2021-01-04 23:45:18 +0100

    Setup

--- tests/tests_bitmap.c
+++ tests/tests_bitmap.c
@@ -2,8 +2,21 @@
 // Created by lara on 04/01/2021.
 //
 
+#include "utils.h"
+#include "bitmap.h"
+
+void testCase1(){
+
+    char filepath[] = "all_gray.bmp";
+
+    FILE * bmp = fopen(filepath, "rb");
+
+    fseek(bmp, 0, SEEK_END);
+    uint32_t file_size = ftell(bmp);
+    rewind(bmp);
+}
 
 int main(){
-
+    testCase1();
     return 0;
 }
