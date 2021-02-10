25083aea4a4e12f06a40cb6ce54731e9e7ce63c0
larapollehn <lara.pollehn@googlemail.com>
2021-02-10 22:35:58 +0100

    CLI setup

---
+++ include/cli.h
@@ -0,0 +1,14 @@
+//
+// Created by lara on 10.02.21.
+//
+
+#ifndef LIBRARY_CLI_H
+#define LIBRARY_CLI_H
+
+char * features[] = {"create & draw", "transform 8bpp to 24bpp", "print image details", "duplicate", "grayscale"};
+
+char * drawOptions[] = {"circle", "rectangle", "triangle"};
+
+char * drawCircle[] = {"radius", "center point P(x,y): x", "center point P(x,y): y"};
+
+#endif //LIBRARY_CLI_H
